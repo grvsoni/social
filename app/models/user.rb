d8990a2c4a123c438eca42141a1b348c39289385
gaurav <raveformails@gmail.com>
2012-12-04 21:31:51 +0530

    reverted change2

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,7 @@ class User < ActiveRecord::Base
     access_token = auth.credentials.token
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     puts "change 3"
-    puts "change 2"
-	  unless user
+    unless user
 	  	user = User.where(:email => auth.info.email).first
 	  	if user
 	  		user.provider = auth.provider

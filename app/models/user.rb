7d67255073ee2714da262461b8c0f866fe6190c6
gaurav <raveformails@gmail.com>
2012-12-04 21:31:14 +0530

    changes 1 to 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     access_token = auth.credentials.token
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    puts "change 1"
+    puts "change 3"
     puts "change 2"
 	  unless user
 	  	user = User.where(:email => auth.info.email).first

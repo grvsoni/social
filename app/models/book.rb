9ab1a892bf20e516de48ed0e26ea10b5fc2925b6
gaurav <raveformails@gmail.com>
2012-12-05 12:11:09 +0530

    unused method rmoved

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,3 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :date_of_publish, :name, :price, :publiser, :summary
-
-  def method1
-  	puts "1"
-  end
-
-  def method2
-  	puts "3"
-  end  
 end
